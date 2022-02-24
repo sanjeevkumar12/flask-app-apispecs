bec2d2b619500fa81f442c7d7ab45829ebb72ed8
Sanjeev Kumar <sanjumassal@gmail.com>
2022-02-24 11:03:56 +0530

    User Login

--- auth/api/schema/register.py
+++ auth/api/schema/register.py
@@ -1,4 +1,4 @@
-from marshmallow import fields
+from marshmallow import ValidationError, fields, validate, validates_schema
 
 from .login import LoginSchema
 
@@ -8,8 +8,17 @@ class RegisterSchema(LoginSchema):
     email = fields.Email(required=True)
     first_name = fields.String(required=True)
     last_name = fields.String(required=True)
-    password = fields.String(required=True, load_only=True)
+    password = fields.String(
+        required=True, load_only=True, validate=[validate.Length(min=6, max=36)]
+    )
     confirm_password = fields.String(required=True)
+
+    @validates_schema
+    def validate_numbers(self, data, **kwargs):
+        if not data["password"] == data["confirm_password"]:
+            raise ValidationError(
+                {"confirm_password": "Password doesn't matches with confirm password"}
+            )
 
     class Meta:
         ordered = True
